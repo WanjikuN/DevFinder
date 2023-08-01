a5b71b78ee80926794a4c966a1da866ea19cae1d
WanjikuN <cikunjoroge4@gmail.com>
2023-08-01 13:46:23 +0300

    Add search display section

--- src/script.js
+++ src/script.js
@@ -16,3 +16,49 @@ namesList.appendChild(li);
 form.reset();
 };
 form.addEventListener('submit', handleForm);
+
+// Fetching user details on search
+
+const handleSearch =()=>{
+    
+    let listy = document.getElementsByClassName('listy');
+    let listyArray = [...listy];
+    listyArray.forEach(l =>{
+        
+        // extract username
+        let uname = l.textContent; 
+        function extractUsername(uname) {
+            if (uname.includes('github.com/')) {
+              const urlObject = new URL(uname);
+              return urlObject.pathname.split('/')[1];
+            } else {
+              return uname.trim();
+            }
+          } 
+        console.log(extractUsername(uname))
+        let username = extractUsername(uname);
+         // Get method
+        //conume Github api to get user details
+        fetch(`https://api.github.com/users/${username}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let results = document.getElementById('search_results');
+            let div = document.createElement('div');
+            div.setAttribute('id', 'results_div');
+            div.innerHTML = `
+                <div id="nav">
+                <img  src="${data.avatar_url} class="avatar">
+                <div>
+                <h3>${data.login}</h3>
+                <p>Bio: ${data.bio}</p>
+                <p>Public Repos: ${data.public_repos}</p>
+                </div>
+                </div>
+            `
+            results.appendChild(div);
+        })
+
+    })
+    
+}
