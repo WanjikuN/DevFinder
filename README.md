afe5038cf4011333e5768db14a9d454679517d49
WanjikuN <cikunjoroge4@gmail.com>
2023-08-01 10:56:47 +0300

    Add submit event

---
+++ README.md
@@ -0,0 +1,10 @@
+# DEVFINDER
+
+## Desccription
+
+This is a platform that empowers tech-recruiters to 
+easily search, discover and rank the top talented developers 
+from a pool of applicants based on the programming languages previously and often used 
+as well as their GitHub activity.
+
+## Setup
---
+++ src/script.js
@@ -0,0 +1,17 @@
+// create a search form
+    // Adds elements to a ul li 
+    // searches names from the ul
+// A form to add the search inputs
+let namesList = document.getElementById('names_list');
+let form = document.getElementsByTagName('form')[0];
+
+
+const handleForm = (e) => {
+    e.preventDefault();
+let user = document.getElementById('user').value;
+let li = document.createElement('li');
+li.textContent = user;
+namesList.appendChild(li);
+form.reset();
+};
+form.addEventListener('submit', handleForm);
