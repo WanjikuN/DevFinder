ecce5a837ddec0f87084ee50e4acb97e511ed6e9
WanjikuN <cikunjoroge4@gmail.com>
2023-08-02 05:38:50 +0300

    Update styling on list

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@
 // A form to add the search inputs
 let namesList = document.getElementById('names_list');
 let form = document.getElementsByTagName('form')[0];
-
+let latestLength = 0;
 
 const handleForm = (e) => {
     e.preventDefault();
@@ -12,7 +12,11 @@ let user = document.getElementById('user').value;
 let li = document.createElement('li');
 li.textContent = user;
 li.setAttribute('class','listy')
-namesList.appendChild(li);
+// namesList.appendChild(li);
+namesList.insertBefore(li, namesList.firstChild); // Insert the latest item at the top
+latestLength++; // Increment the latest length for the next item
+li.innerHTML = latestLength + ". "+  user; // Set the text content with the latest length
+
 form.reset();
 };
 form.addEventListener('submit', handleForm);
@@ -26,7 +30,7 @@ const handleSearch =()=>{
     listyArray.forEach(l =>{
         
         // extract username
-        let uname = l.textContent; 
+        let uname = l.textContent.split(" ")[1]; 
         function extractUsername(uname) {
             if (uname.includes('github.com/')) {
               const urlObject = new URL(uname);
@@ -57,6 +61,9 @@ const handleSearch =()=>{
                 </div>
             `
             results.appendChild(div);
+            div.addEventListener('click', ()=>{
+                languagesDisplay(username);
+            })
 
         })
         
@@ -67,3 +74,31 @@ const handleSearch =()=>{
 const clearListArea = () => {
     namesList.innerHTML = ''; 
   };
+
+// on user click display their most used languages
+function languagesDisplay(user){
+    // Add this code to your JavaScript file
+const apiUrl = 'https://cors-anywhere.herokuapp.com/https://github-readme-stats.vercel.app/api/top-langs?username=WanjikuN&locale=en&langs_count=8';
+
+// Fetch the data from the GitHub API
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Process the fetched data
+    const languagesContainer = document.getElementById('languages-container');
+
+    // Create a list to display the languages and their usage percentage
+    const list = document.createElement('ul');
+    for (const language in data) {
+      const item = document.createElement('li');
+      item.textContent = `${language}: ${data[language]}%`;
+      list.appendChild(item);
+    }
+
+    // Add the list to the container
+    languagesContainer.appendChild(list);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+}
